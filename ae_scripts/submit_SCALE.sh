f4fad574c9ead9cd0b57d682329e6a42bb91d128
Linus-Lee-1037 <1140874687@qq.com>
2026-07-17 14:49:50 -0400

    update submit_SCALE.sh

--- ae_scripts/submit_SCALE.sh
+++ ae_scripts/submit_SCALE.sh
@@ -3,5 +3,5 @@ data_dict_path=$1
 variables=(PRES QC QG QI QR QS QV RH T U V W)
 
 for var in "${variables[@]}"; do
-   sbatch ./ac_scripts/SCALE_ablation_study.sh $data_dict_path $var
+   sbatch ./ae_scripts/SCALE_ablation_study.sh $data_dict_path $var
 done
