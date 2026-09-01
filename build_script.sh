2e3694bc4e89f0d7a7d72b8f96db0ab4e835d020
Xin Liang <xin.liang@oregonstate.edu>
2026-09-01 14:22:29 -0700

    update build script

--- build_script.sh
+++ build_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 export CMAKE_POLICY_VERSION_MINIMUM=3.5
 # Compilers: uses the system default unless overridden, e.g.
@@ -7,42 +8,60 @@ export CMAKE_POLICY_VERSION_MINIMUM=3.5
 # since CMake caches the compiler on the first configure.
 export CC=${CC:-gcc}
 export CXX=${CXX:-g++}
+
+# Pinned dependency versions (tested together; QoZ headers must match SZ3's)
+SZ2_COMMIT=308bd06f0040ec0d5c22fb3fcb0428c306ba4df1
+SZ3_COMMIT=90c66bed1c04e701442ecb104b912548fcfabee9
+QOZ_COMMIT=17f124ef9a341f4bd661c20e6f0a3e80b19c43ef
+MGARD_COMMIT=7ba6738d429a70da8bd1d345ac7aad702366f16b
+
+# clone (if needed) and check out the pinned commit
+fetch_dependency() {
+    name=$1; url=$2; commit=$3
+    if [ ! -d "${name}" ]; then
+        git clone "${url}" "${name}"
+    fi
+    cd "${name}"
+    git cat-file -e "${commit}^{commit}" 2>/dev/null || git fetch origin
+    git reset --hard "${commit}"
+    cd ..
+}
+
 source_dir=`pwd`
 external_dir=${source_dir}/external
 mkdir -p external
 cd ${external_dir}
 
 # build SZ2
-git clone https://github.com/szcompressor/SZ2.git
+fetch_dependency SZ2 https://github.com/szcompressor/SZ2.git ${SZ2_COMMIT}
 cd SZ2
 mkdir -p build
 mkdir -p install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${external_dir}/SZ2/install ..
-make -j
+make -j 8
 make install
 
 # build SZ3
 cd ${external_dir}
-git clone https://github.com/szcompressor/SZ3.git
+fetch_dependency SZ3 https://github.com/szcompressor/SZ3.git ${SZ3_COMMIT}
 cd SZ3
-git reset --hard 90c66bed1c04e701442ecb104b912548fcfabee9
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${external_dir}/SZ3/install ..
-make -j
+make -j 8
 make install
 
-# build QoZ (HPEZ)
+# build QoZ (HPEZ) -- header-only; ska_hash is excluded because QoZ reuses SZ3's copy
 cd ${external_dir}
-git clone https://github.com/Linus-Li-1037/QoZ.git
+fetch_dependency QoZ https://github.com/Linus-Li-1037/QoZ.git ${QOZ_COMMIT}
 cd QoZ
 mkdir -p install/include/
-rsync -av --exclude 'ska_hash' include/ ${external_dir}/QoZ/install/include/
+rsync -a --exclude 'ska_hash' include/ ${external_dir}/QoZ/install/include/
 
 # build MGARD
 cd ${external_dir}
-git clone https://github.com/CODARcode/MGARD.git
+fetch_dependency MGARD https://github.com/CODARcode/MGARD.git ${MGARD_COMMIT}
 cd MGARD
 sh build_scripts/build_mgard_serial.sh 8
 
@@ -63,4 +82,3 @@ make -j 8
 # cmake -DADIOS2_USE_MPI=OFF -DADIOS2_USE_SZ=OFF -DCMAKE_INSTALL_PREFIX=${external_dir}/ADIOS2/adios2-install ..
 # make -j 8
 # make install
-
